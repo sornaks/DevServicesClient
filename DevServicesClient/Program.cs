283c2a6027e6b06287538092971cd79c386cddf9
Sornakumar Sundararajan <sornaks@microsoft.com>
2017-12-10 20:23:00 -0800

    Adding a simple dev services client.

---
+++ DevServicesClient/Program.cs
@@ -0,0 +1,130 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------
+
+namespace DevServicesClient
+{
+    using System;
+    using System.IO;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args == null)
+            {
+                PrintHelp();
+                return;
+            }
+
+            if (args.Length < 4)
+            {
+                PrintHelp();
+                throw new ArgumentException("Invalid arguments.");
+            }
+
+            var managementCertificateThumbprint = args[0];
+            var operation = args[1];
+            var subscriptionId = args[2];
+            var cloudServiceName = args[3];
+
+            if (string.IsNullOrWhiteSpace(managementCertificateThumbprint))
+            {
+                throw new ArgumentNullException("managementCertificateThumbprint");
+            }
+
+            if (string.IsNullOrEmpty(operation))
+            {
+                throw new ArgumentNullException("Please specify operation.");
+            }
+
+            if (string.IsNullOrWhiteSpace(subscriptionId))
+            {
+                throw new ArgumentNullException("subscriptionId");
+            }
+
+            if (string.IsNullOrWhiteSpace(cloudServiceName))
+            {
+                throw new ArgumentNullException("cloudServiceName");
+            }
+
+            var rdfeClient = new RDFEClient(
+                managementCertificateThumbprint: args[0],
+                subscriptionId: args[2],
+                cloudServiceName: args[3]);
+
+            if (operation.Equals("GetCloudService", StringComparison.OrdinalIgnoreCase))
+            {
+                rdfeClient.GetCloudService();
+                return;
+            }
+
+            var resourceProviderNamespace = args[4];
+            var resourceType = args[5];
+            var resourceName = args[6];
+
+            if (string.IsNullOrWhiteSpace(resourceProviderNamespace))
+            {
+                throw new ArgumentNullException("resourceProviderNamespace");
+            }
+
+            if (string.IsNullOrWhiteSpace(resourceType))
+            {
+                throw new ArgumentNullException("resourceType");
+            }
+
+            if (string.IsNullOrWhiteSpace(resourceName))
+            {
+                throw new ArgumentNullException("resourceName");
+            }
+
+            if (operation.Equals("GetResource", StringComparison.OrdinalIgnoreCase))
+            {
+                rdfeClient.GetDevService(resourceName);
+                return;
+            }
+
+            if (operation.Equals("Update", StringComparison.OrdinalIgnoreCase))
+            {
+                var pathToXMLDefinition = args[8];
+                if (string.IsNullOrWhiteSpace(pathToXMLDefinition))
+                {
+                    throw new ArgumentNullException("pathToXMLDefinition");
+                }
+
+                if (!File.Exists(pathToXMLDefinition))
+                {
+                    throw new ArgumentException("Unable to read file. It is likely that the path is incorrect.");
+                }
+
+                rdfeClient.UpdateDevService(resourceProviderNamespace, resourceType, resourceName, pathToXMLDefinition);
+                return;
+            }
+
+            if (operation.Equals("Delete", StringComparison.OrdinalIgnoreCase))
+            {
+                rdfeClient.DeleteDevService(resourceProviderNamespace, resourceType, resourceName);
+                return;
+            }
+
+            if (operation.Equals("GetSsoToken", StringComparison.OrdinalIgnoreCase))
+            {
+                rdfeClient.GetSsoToken(resourceProviderNamespace, resourceType, resourceName);
+                return;
+            }
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("================ HELP ==================");
+            Console.WriteLine("Argument 1: Management certificate thumbprint.");
+            Console.WriteLine("Argument 2: Operation. Supported values: GetCloudService, GetResource, Update, Delete, GetSsoToken");
+            Console.WriteLine("Argument 3: Subscription ID.");
+            Console.WriteLine("Argument 4: Cloud service name.");
+            Console.WriteLine("Argument 5: Resource provider namespace.");
+            Console.WriteLine("Argument 6: Resource type.");
+            Console.WriteLine("Argument 7: Resource name.");
+            Console.WriteLine("Argument 8: Path to resource file to update the resource definition.");
+        }
+    }
+}
---
+++ DevServicesClient/RDFEClient.cs
@@ -0,0 +1,194 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------
+
+namespace DevServicesClient
+{
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Xml;
+    using System.Xml.Linq;
+
+    public class RDFEClient
+    {
+        private const string XMSVersionHeader = "x-ms-version";
+
+        private const string XMSVersionHeaderValue = "2014-12-01";
+
+        private const string ManagementUrl = "https://management.core.windows.net/";
+
+        public string ManagementCertificateThumbprint { get; private set; }
+
+        public string SubscriptionId { get; private set; }
+
+        public string CloudServiceName { get; private set; }
+
+        public RDFEClient(string managementCertificateThumbprint, string subscriptionId, string cloudServiceName)
+        {
+            this.ManagementCertificateThumbprint = managementCertificateThumbprint;
+            this.SubscriptionId = subscriptionId;
+            this.CloudServiceName = cloudServiceName;
+        }
+
+        public XElement GetCloudService(bool shouldPrint = true)
+        {
+            using (var handler = new WebRequestHandler())
+            {
+                handler.ClientCertificates.Add(this.GetCertificate());
+                using (var client = new HttpClient(handler))
+                {
+                    var request = new HttpRequestMessage(
+                        HttpMethod.Get,
+                        ManagementUrl + this.SubscriptionId +
+                        "/cloudservices/" + this.CloudServiceName);
+                    request.Headers.Add(XMSVersionHeader, XMSVersionHeaderValue);
+
+                    var response = client.SendAsync(request).Result;
+
+                    if (shouldPrint)
+                    {
+                        Console.WriteLine("Status Code:" + response.StatusCode);
+                    }
+
+                    var responseContent = response.Content.ReadAsStringAsync().Result;
+                    if (!string.IsNullOrWhiteSpace(responseContent))
+                    {
+                        var parsedResponse = XElement.Parse(responseContent);
+                        if (shouldPrint)
+                        {
+                            Console.WriteLine(parsedResponse);
+                        }
+
+                        return parsedResponse;
+                    }
+
+                    return null;
+                }
+            }
+        }
+
+        public void GetDevService(string resourceName)
+        {
+            var cloudService = this.GetCloudService(shouldPrint: false);
+            if (cloudService != null)
+            {
+                var document = new XmlDocument();
+                document.LoadXml(cloudService.ToString());
+
+                var ns = new XmlNamespaceManager(document.NameTable);
+                ns.AddNamespace("azure", "http://schemas.microsoft.com/windowsazure");
+
+                var resource = document.SelectSingleNode("//azure:Resource[azure:Name='" + resourceName + "']", ns);
+                Console.WriteLine(XElement.Parse(resource.OuterXml));
+            }
+        }
+
+        public void DeleteDevService(string resourceProviderNamespace, string resourceType, string resourceName)
+        {
+            using (var handler = new WebRequestHandler())
+            {
+                handler.ClientCertificates.Add(this.GetCertificate());
+                using (var client = new HttpClient(handler))
+                {
+                    var request = new HttpRequestMessage(
+                        HttpMethod.Delete,
+                        ManagementUrl + this.SubscriptionId +
+                        "/cloudservices/" + this.CloudServiceName +
+                        "/resources/" + resourceProviderNamespace +
+                        "/" + resourceType +
+                        "/" + resourceName);
+                    request.Headers.Add(XMSVersionHeader, XMSVersionHeaderValue);
+
+                    var response = client.SendAsync(request).Result;
+                    Console.WriteLine("Status Code:" + response.StatusCode);
+
+                    var responseContent = response.Content.ReadAsStringAsync().Result;
+                    if (!string.IsNullOrWhiteSpace(responseContent))
+                    {
+                        var parsedResponse = XElement.Parse(responseContent);
+                        Console.WriteLine(parsedResponse);
+                    }
+                }
+            }
+        }
+
+        public void UpdateDevService(string resourceProviderNamespace, string resourceType, string resourceName, string pathToXMLDefinition)
+        {
+            var xmlContent = File.ReadAllLines(pathToXMLDefinition);
+
+            using (var handler = new WebRequestHandler())
+            {
+                handler.ClientCertificates.Add(this.GetCertificate());
+                using (var client = new HttpClient(handler))
+                {
+                    var request = new HttpRequestMessage(
+                        HttpMethod.Put,
+                        ManagementUrl + this.SubscriptionId +
+                        "/cloudservices/" + this.CloudServiceName +
+                        "/resources/" + resourceProviderNamespace +
+                        "/" + resourceType +
+                        "/" + resourceName);
+                    request.Headers.Add(XMSVersionHeader, XMSVersionHeaderValue);
+
+                    var response = client.SendAsync(request).Result;
+                    Console.WriteLine("Status Code:" + response.StatusCode);
+
+                    var responseContent = response.Content.ReadAsStringAsync().Result;
+                    if (!string.IsNullOrWhiteSpace(responseContent))
+                    {
+                        var parsedResponse = XElement.Parse(responseContent);
+                        Console.WriteLine(parsedResponse);
+                    }
+                }
+            }
+        }
+
+        public void GetSsoToken(string resourceProviderNamespace, string resourceType, string resourceName)
+        {
+            using (var handler = new WebRequestHandler())
+            {
+                handler.ClientCertificates.Add(this.GetCertificate());
+                using (var client = new HttpClient(handler))
+                {
+                    var request = new HttpRequestMessage(
+                        HttpMethod.Post,
+                        ManagementUrl + this.SubscriptionId +
+                        "/cloudservices/" + this.CloudServiceName +
+                        "/resources/" + resourceProviderNamespace +
+                        "/" + resourceType +
+                        "/" + resourceName +
+                        "/SsoToken");
+                    request.Headers.Add(XMSVersionHeader, XMSVersionHeaderValue);
+
+                    var response = client.SendAsync(request).Result;
+
+                    Console.WriteLine("Status Code:" + response.StatusCode);
+
+                    var responseContent = response.Content.ReadAsStringAsync().Result;
+                    if (!string.IsNullOrWhiteSpace(responseContent))
+                    {
+                        var parsedResponse = XElement.Parse(responseContent);
+                        Console.WriteLine(parsedResponse);
+                        Console.WriteLine("The URI is URL encoded. Please decode it before use.");
+                    }
+                }
+            }
+        }
+
+        private X509Certificate2 GetCertificate()
+        {
+            var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly);
+            var certs = store.Certificates.Find(X509FindType.FindByThumbprint, this.ManagementCertificateThumbprint, false);
+
+            if (certs.Count < 1)
+            {
+                throw new InvalidOperationException("Please install the management certificate in CurrentUser\\My");
+            }
+
+            return certs[0];
+        }
+    }
+}
